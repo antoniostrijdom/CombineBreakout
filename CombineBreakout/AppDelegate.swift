739d8f79b60aab722200c44388c5be138f72f86a
Antonio Strijdom <>
2019-10-13 14:32:12 +0100

    Initial commit.

---
+++ CombineBreakout/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  CombineBreakout
+//
+//  Created by Antonio Strijdom on 13/10/2019.
+//  Copyright © 2019 Antonio Strijdom. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
